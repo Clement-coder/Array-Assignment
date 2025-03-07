fc99c81a74c1161a0112400bc1e25535dde3765d
Clement Raymond <chinexzy37@gmail.com>
2025-03-07 14:03:03 +0000

    initial commit

--- celcis.js
+++ celcis.js
@@ -1,12 +1,14 @@
 // Create a program that converts celcius to fahrenheit and vice versa
 
-function celciusToFahrenheit(celcius){
-    return(celcius * 9/5) +32;
-}
-console.log(`25°C =${celciusToFahrenheit(25)}°F`);
+// function celciusToFahrenheit(celcius){
+//     return(celcius * 9/5) +32;
+// }
+// console.log(`25°C =${celciusToFahrenheit(25)}°F`);
 
-function fahrenheitToCelsius(fahrenheit) {
-    return (fahrenheit - 32) * 5/9;
-}
+// function fahrenheitToCelsius(fahrenheit) {
+//     return (fahrenheit - 32) * 5/9;
+// }
 
-console.log(`77°F = ${fahrenheitToCelsius(77)}°C`);
+// console.log(`77°F = ${fahrenheitToCelsius(77)}°C`);
+console.log(25*9/5+32)
+console.log((77-32)*5/9)
